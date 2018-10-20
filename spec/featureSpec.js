27138bf7cc2efce3e7478208913121affcbe8101
Rashika Patel <rashika_patel@hotmail.com>
2018-10-20 12:23:19 +0100

    add down function to decrease temperature

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -11,9 +11,14 @@ describe("using thermostat", function() {
     expect(thermostat.getCurrentTemp()).toEqual(20);
   });
 
-  it("should increase the temperture by 1", function() {
+  it("should increase the temperature by 1", function() {
     thermostat.up();
     expect(thermostat.getCurrentTemp()).toEqual(21);
+  });
+
+  it("should decrease the temperature by 1", function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemp()).toEqual(19);
   });
 
 });
--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,12 +7,17 @@ describe("Thermostat", function() {
     thermostat = new Thermostat;
   });
 
-  it("should return temperture as 20", function() {
+  it("should return temperature as 20", function() {
     expect(thermostat.getCurrentTemp()).toEqual(20);
   });
 
   it("should return temperature as 21", function() {
     thermostat.up();
     expect(thermostat.getCurrentTemp()).toEqual(21);
+  });
+
+  it("should return temperature as 19", function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemp()).toEqual(19);
   });
 });
