72acb5b4bccf550d32a30377350889b87cf3b0a5
Rashika Patel <rashika_patel@hotmail.com>
2018-10-20 20:45:47 +0100

    add connection to weather api and display current weather

--- src/thermostatController.js
+++ src/thermostatController.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   $('#mode').on('click', function() {
     thermostat.switchPowerSavingMode()
     updateDisplay()
-  })
+  });
 
   function updateDisplay() {
     $('#temperature').text(thermostat.getCurrentTemp());
@@ -28,10 +28,12 @@ $(document).ready(function() {
     $('#mode').attr('class', `psm-${thermostat.isPowerSavingOn()}`)
   }
 
+});
 
+$.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=' + apikey, function(data) {
+	//I think the problem was that we were calling in this file before the passwords file. The order of the scripts would appear to matter.
 
-
-
-
-
-})
+  $('#outside-temp').text(Math.floor(data.main.temp -273.15) + " C")
+  var weather = data.weather[0].description
+  	$('#weather').text(weather.charAt(0).toUpperCase() + weather.slice(1))
+});
